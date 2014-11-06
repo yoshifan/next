092180a3dedfa420e64134acc7f7adecc4657c02
Ricardo Mendes <rokusu@gmail.com>
2014-11-06 05:06:34 +0000

    Adds tests, bootstrap, ember-data-hal-adapter

---
+++ app/adapters/application.js
@@ -0,0 +1,8 @@
+// import DS from 'ember-data';
+//
+// export default DS.RESTAdapter.extend({
+// });
+import Adapter from 'ember-data-hal-adapter/hal-adapter';
+
+export default Adapter.extend({
+});

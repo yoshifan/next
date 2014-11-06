2132cde5e73e6b534171247eddc0d6d49561bc13
Ricardo Mendes <rokusu@gmail.com>
2014-11-06 05:05:38 +0000

    Updates router

--- app/router.js
+++ app/router.js
@@ -6,7 +6,11 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('games', function() { });
+  this.resource('game', { path: 'games/:game_id' }, function() { });
+  this.resource('player', { path: 'players/:player_id' }, function() { });
   this.resource('fzgx/randomizer', function() { });
+  this.route('fzmv/faq');
 });
 
 export default Router;

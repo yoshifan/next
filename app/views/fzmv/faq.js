996c04c21048bf4ef708e6d0a56f9aac8451a374
Ricardo Mendes <rokusu@gmail.com>
2014-11-20 05:04:53 +0000

    Updates React.renderComponent to React.render

--- app/views/fzmv/faq.js
+++ app/views/fzmv/faq.js
@@ -3,6 +3,8 @@ import JetVermillionCode from '../../react/jet-vermillion-code';
 
 export default Ember.View.extend({
   didInsertElement: function() {
-    React.renderComponent(React.createElement(JetVermillionCode, null), this.$('.generator')[0]);
+    React.render(React.createElement(JetVermillionCode, null), this.$('.generator')[0]);
   }
 });
+
+

afeb24d0fa6ac43640c7216a6903fccdd0cf8a2d
Ricardo Mendes <rokusu@gmail.com>
2015-01-28 23:14:15 +0000

    Replaces React component with Ember component

---
+++ app/components/jet-vermillion-code.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+import jetVermillionGenerator from 'npm:jet-vermillion-generator';
+
+export default Ember.Component.extend({
+  classNames: ['generator'],
+
+  username: '',
+  generatedCode: '',
+
+  generateCode: function() {
+    this.set('generatedCode', jetVermillionGenerator.generate(this.get('username')))
+  }.observes('username')
+});

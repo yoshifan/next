6ce37c9084ebb719167b4512b099057d188448ab
Ricardo Mendes <rokusu@gmail.com>
2015-01-28 23:13:40 +0000

    Turns off importing of JS from ember-cli-bootstrap

--- Brocfile.js
+++ Brocfile.js
@@ -4,7 +4,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 var pickFiles = require('broccoli-static-compiler');
 
 
-var app = new EmberApp();
+var app = new EmberApp({
+  'ember-cli-bootstrap': {
+    'importBootstrapJS': false
+  }
+});
 
 // Use `app.import` to add additional libraries to the generated
 // output files.

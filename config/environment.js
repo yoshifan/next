ae50019db7fc45815780e7f267fe492b32b36857
Ricardo Mendes <rokusu@gmail.com>
2015-01-29 01:54:56 +0000

    Moves components to pod structure

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,8 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'fzerocentral-web',
+    podModulePrefix: 'fzerocentral-web/pods',
+    usePodsByDefault: true,
     environment: environment,
     baseURL: '/',
     locationType: 'auto',

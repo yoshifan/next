303835a8c685fffa7ac2cb831f9f59ea60f4a039
Ricardo Mendes <rokusu@gmail.com>
2015-07-14 05:28:43 +0100

    Moves player model

---
+++ app/models/player.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+const { Model, attr, hasMany } = DS;
+
+export default Model.extend({
+  username: attr(),
+  country: attr(),
+  avatar: attr(),
+
+  records: hasMany('record', { async: true })
+});
--- app/pods/player/model.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  username: DS.attr(),
-  country: DS.attr(),
-  avatar: DS.attr(),
-
-  records: DS.hasMany()
-});

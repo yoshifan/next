4601af67dfe21306cbf5a4e761d6ff81f3e0c2c8
Ricardo Mendes <rokusu@gmail.com>
2015-08-20 20:04:50 +0100

    Creates group-by helper

---
+++ app/helpers/group-by.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+
+const {
+  A,
+  computed,
+  get,
+  isPresent
+} = Ember;
+
+export function groupBy(params/*, hash*/) {
+  let [ collection, property ] = params;
+  let groups = new A();
+  let items = collection;
+
+  items.forEach(function(item) {
+    let value = get(item, property);
+    let group = groups.findBy('value', value);
+
+    if (isPresent(group)) {
+      get(group, 'items').push(item);
+    } else {
+      group = { property: property, value: value, items: [item] };
+      groups.push(group);
+    }
+  });
+
+  return groups;
+}
+
+export default Ember.Helper.helper(groupBy);
---
+++ tests/unit/helpers/group-by-test.js
@@ -0,0 +1,10 @@
+import { groupBy } from '../../../helpers/group-by';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | group by');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = groupBy(42);
+  assert.ok(result);
+});
